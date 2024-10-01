36bc205640c68bc7c610616d05a6eea98e291ac7
meskatabusayed <meskatabusayed@gmail.com>
2024-10-01 21:04:00 +0530

    math need for learning computer program

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 //### : Milestone - 4 : Hello Javascript
-//## Module 16 : Introduction to javascript
+//## Module 16 .1 : Introduction to javascript
 //Q1 : What is Javascript ?
 /*===> source code =>(interpreter) => Mechine code (interpreter) => result */
 /* Answer : Javascript is high level , interpreted programming language used to make web pages more interactive.It help's implement complex and beautiful things/design on web pages.*/
@@ -12,9 +12,44 @@
 -> Now standardized by the European computer Manufectures Assoiciation as ECMA-262
 -> An HTML - embaddeed scripting language
 -> we'II call collections of javascript code scripts, not programs.
+*/
 
+/* Q3 : How does javascript run on the browser? */
+
+/* Q4: Javascript Applications : 
+1. website
+2. web Application
+3. Presentation
+4. server Application
+5. web servers
+6. Games
+7. Art
+8. smartwatch Apps
+9. Mobile Apps
+10. Flying Robots
+*/
+/* Q5 :  */
+
+//# 16.2 : Math Needed to start learning programming language:
+/* 
+1. Number -> 55, 66, -4, 9.6
+2. Integer Number -> 5, 6, 7
+3. Float number -< 32.4 , 56.4
+4. Operation -> addition , subtraction , multiplication , division, remainder
+5. Percent 
+6. Average
+7. comparision -> < , > , >= , <= , == , !=
+8. okik rules Math
+9. 32 , 43 , 56 => max , min , avarage , sum
+10. series
+* Extra:
+----------
+1. Prime number
+2. Permutation
+"""Always jodi lage shikhee nebo..."""
 
 */
-const a = 20;
-console.log(a);
+
+//#16.3 : what is variable , Five things you need to declare A Variable
+
 
