bf4da8108258c6df0fa40c88c11100a26cfa20b2
meskatabusayed <meskatabusayed@gmail.com>
2024-10-01 19:34:33 +0530

    some basic question added

---
+++ javascript.js
@@ -0,0 +1,20 @@
+//### : Milestone - 4 : Hello Javascript
+//## Module 16 : Introduction to javascript
+//Q1 : What is Javascript ?
+/*===> source code =>(interpreter) => Mechine code (interpreter) => result */
+/* Answer : Javascript is high level , interpreted programming language used to make web pages more interactive.It help's implement complex and beautiful things/design on web pages.*/
+//script means - olpo code a onek kaj...
+//Q2 : History of javascript?
+/* Answer : Javascript was invanted by Brendan Eich.Netscape 2 was released with javascript 1.0.
+//Origins of javascript : 
+-> Originally developed by Netscape , as Livescript .
+-> Become a joint venture of Netscape and sun in 1995, renamed javascript.
+-> Now standardized by the European computer Manufectures Assoiciation as ECMA-262
+-> An HTML - embaddeed scripting language
+-> we'II call collections of javascript code scripts, not programs.
+
+
+*/
+const a = 20;
+console.log(a);
+
